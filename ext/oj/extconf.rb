6fe009f43cd3968fc649ec3ced83fa8ee12cf77c
Peter Ohler <peter@ohler.com>
2012-10-17 20:30:43 -0700

    using rbconfig in extconf.rb

--- ext/oj/extconf.rb
+++ ext/oj/extconf.rb
@@ -1,11 +1,14 @@
 require 'mkmf'
+require 'rbconfig'
 
 extension_name = 'oj'
 dir_config(extension_name)
 
 parts = RUBY_DESCRIPTION.split(' ')
 type = parts[0]
+type = type[4..-1] if type.start_with?('tcs-')
 type = 'ree' if 'ruby' == type && RUBY_DESCRIPTION.include?('Ruby Enterprise Edition')
+is_windows = RbConfig::CONFIG['host_os'] =~ /(mingw|mswin)/ ? true : false
 platform = RUBY_PLATFORM
 version = RUBY_VERSION.split('.')
 puts ">>>>> Creating Makefile for #{type} version #{RUBY_VERSION} on #{platform} <<<<<"
@@ -26,8 +29,8 @@ dflags = {
   'HAS_EXCEPTION_MAGIC' => ('ruby' == type && ('1' == version[0] && '9' == version[1]) || '2' <= version[0]) ? 0 : 1,
   'HAS_PROC_WITH_BLOCK' => ('ruby' == type && ('1' == version[0] && '9' == version[1]) || '2' <= version[0]) ? 1 : 0,
   'HAS_TOP_LEVEL_ST_H' => ('ree' == type || ('ruby' == type &&  '1' == version[0] && '8' == version[1])) ? 1 : 0,
-  'IS_WINDOWS' => ('i386-mingw32' == platform || 'tcs-ruby' == type) ? 1 : 0,
-  'SAFE_CACHE' => ('i386-mingw32' == platform || 'tcs-ruby' == type) ? 0 : 1,
+  'IS_WINDOWS' => is_windows ? 1 : 0,
+  'SAFE_CACHE' => is_windows ? 0 : 1,
 }
 # This is a monster hack to get around issues with 1.9.3-p0 on CentOS 5.4. SO
 # some reason math.h and string.h contents are not processed. Might be a
@@ -38,7 +41,7 @@ if 'x86_64-linux' == RUBY_PLATFORM && '1.9.3' == RUBY_VERSION && '2011-10-30' ==
   rescue Exception => e
   end
 else
-  dflags['NEEDS_STPCPY'] = nil if 'i386-mingw32' == platform || 'tcs-ruby' == type
+  dflags['NEEDS_STPCPY'] = nil if is_windows
 end
 
 dflags.each do |k,v|
