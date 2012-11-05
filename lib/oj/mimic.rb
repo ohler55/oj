e703f7965a2b68d001f4a8ef939eee334192a860
Peter Ohler <ohler@mac.com>
2012-11-05 08:39:52 -0800

    attempting to mimic json_pure

--- lib/oj/mimic.rb
+++ lib/oj/mimic.rb
@@ -4,7 +4,7 @@ module Oj
   def self.mimic_loaded()
     gems_dir = File.dirname(File.dirname(File.dirname(File.dirname(__FILE__))))
     Dir.foreach(gems_dir) do |gem|
-      next unless gem.start_with?('json-')
+      next unless (gem.start_with?('json-') || gem.start_with?('json_pure'))
       $LOADED_FEATURES << File.join(gems_dir, gem, 'lib', 'json.rb')
     end
   end
--- lib/oj/version.rb
+++ lib/oj/version.rb
@@ -1,5 +1,5 @@
 
 module Oj
   # Current version of the module. 
-  VERSION = '1.4.3'
+  VERSION = '1.4.4a1'
 end
