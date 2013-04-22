6bc4f37b954997f1ff1b662571b23dc973ceccbc
Peter Ohler <peter@ohler.com>
2013-04-21 20:44:53 -0700

    improved mimic masking of json

--- lib/oj/mimic.rb
+++ lib/oj/mimic.rb
@@ -7,6 +7,14 @@ module Oj
       next unless (gem.start_with?('json-') || gem.start_with?('json_pure'))
       $LOADED_FEATURES << File.join(gems_dir, gem, 'lib', 'json.rb')
     end
+    # and another approach in case the first did not get all
+    $LOAD_PATH.each do |d|
+      next unless File.exist?(d)
+      Dir.entries(d) do |file|
+        next unless file.ends_with?('jsob.rb')
+        $LOADED_FEATURES << file unless $LOADED_FEATURES.include?(file)
+      end
+    end
     mimic_paths.each { |p| $LOADED_FEATURES << p }
   end
 end
