137f030298a506bd953fde216d0dcb8fd92899bc
Dmitry Krasnoukhov <dmitry@krasnoukhov.com>
2014-06-06 18:31:34 +0300

    Improve $LOADED_FEATURES append pattern
    ```start_with?('json')``` is not correct implementation.
    For example, this prevents ```json_spec``` gem from being loaded.
    Regular expression which checks if there is a file or folder starting
    with json works better.

--- test/isolated/test_mimic_single.rb
+++ test/isolated/test_mimic_single.rb
@@ -9,10 +9,20 @@ class MimicSingle < Minitest::Test
   def test_mimic_single
     assert(defined?(JSON).nil?)
     Oj.mimic_JSON
-    assert(!defined?(JSON).nil?)
-    # Make sure to_json is define for object.
-    {'a' => 1}.to_json()
-    Object.new().to_json()
-  end
 
+    # Test constants
+    assert(!defined?(JSON).nil?)
+    assert(!defined?(JSON::ParserError).nil?)
+
+    # Test loaded features
+    assert(!require('json'))
+
+    begin
+      require('json_spec')
+    rescue LoadError
+      assert(true)
+      return
+    end
+    assert(false, '** should raise LoadError')
+  end
 end # MimicSingle
