988322194319fd5e23d5b289bac37af40e233459
Ian Ker-Seymer <i.kerseymer@gmail.com>
2017-03-15 01:30:35 -0400

    Add more tests for empty_string option

--- test/test_various.rb
+++ test/test_various.rb
@@ -6,6 +6,12 @@ $: << File.dirname(__FILE__)
 require 'helper'
 
 class Juice < Minitest::Test
+  def gen_whitespaced_string(length = Random.new.rand(100))
+    whitespace_chars = [" ", "\t", "\f", "\n", "\r"]
+    result = ""
+    length.times { result << whitespace_chars.sample }
+    result
+  end
 
   module TestModule
   end
@@ -1298,6 +1304,21 @@ class Juice < Minitest::Test
   def test_nilnil_true
     obj = Oj.load(nil, :nilnil => true)
     assert_equal(nil, obj)
+  end
+
+  def test_empty_string_true
+    10.times do
+      result = Oj.load(gen_whitespaced_string, :empty_string => true)
+      assert_nil(result)
+    end
+  end
+
+  def test_empty_string_false
+    10.times do
+      assert_raises(Oj::ParseError) do
+        Oj.load(gen_whitespaced_string, :empty_string => false)
+      end
+    end
   end
 
   def test_quirks_null_mode
