432224a3e323b78b2aae260a876543075181eef0
stereobooster <stereobooster@gmail.com>
2017-03-24 19:21:55 +0100

    more tests for JSON compatibility

--- test/isolated_compatibility/test_compatibility_json.rb
+++ test/isolated_compatibility/test_compatibility_json.rb
@@ -32,10 +32,9 @@ class CompatibilityJson < Minitest::Test
 
   # Compares the results of to_json and Oj.dump. If an exception is raised the
   # exception class is compared but not the exception message.
-  def test_compat_dump
-    Oj.default_options = OJ_JSON_COMPAT
-    JSON_TEST_DATA.each do |key, value|
-
+  TEST_DATA_TO_JSON.each do |key, value|
+    define_method "test_to_#{key}" do 
+      Oj.default_options = OJ_JSON_COMPAT
       json_value = begin
                      value.to_json
                    rescue Exception => e
@@ -53,4 +52,26 @@ class CompatibilityJson < Minitest::Test
       end
     end
   end
+
+  TEST_DATA_GENERATE.each do |key, value|
+    define_method "test_gen_#{key}" do 
+      Oj.default_options = OJ_JSON_COMPAT
+      json_value = begin
+                     JSON.generate(*value)
+                   rescue Exception => e
+                     e
+                   end
+      oj_value = begin
+                   Oj.to_json(*value)
+                 rescue Exception => e
+                   e
+                 end
+      if json_value.is_a?(Exception)
+        assert_equal(json_value.class, oj_value.class, key.to_s)
+      else
+        assert_equal(json_value, oj_value, key.to_s)
+      end
+    end
+  end
+
 end 
