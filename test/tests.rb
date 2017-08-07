7927c5aa81d8859a368854f3c5d0a3a7ab1b7563
yui-knk <spiketeika@gmail.com>
2017-08-07 14:53:48 +0900

    Order required test files by there name
    "test/tests.rb" is a file which runs other test files.
    Ordering required test files helps us to detect wihich
    files are loaded.

--- test/tests.rb
+++ test/tests.rb
@@ -7,17 +7,17 @@ $oj_dir = File.dirname(File.expand_path(File.dirname(__FILE__)))
   $: << File.join($oj_dir, dir)
 end
 
-require 'test_strict'
-require 'test_null'
 require 'test_compat'
-require 'test_object'
 require 'test_custom'
-require 'test_writer'
 require 'test_fast'
-require 'test_hash'
 require 'test_file'
 require 'test_gc'
+require 'test_hash'
+require 'test_null'
+require 'test_object'
 require 'test_saj'
 require 'test_scp'
+require 'test_strict'
 require 'test_various'
 require 'test_wab'
+require 'test_writer'
