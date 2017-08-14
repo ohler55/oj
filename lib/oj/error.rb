dcea9b15f584b8313d363020bfb9caf6771b86b5
Ashwin Maroli <ashmaroli@gmail.com>
2017-08-14 10:17:06 +0530

    create empty classes with `Class.new`

--- lib/oj/error.rb
+++ lib/oj/error.rb
@@ -1,23 +1,22 @@
 
 module Oj
 
-  class Error < StandardError
-  end # Error
+  # Inherit Error class from StandardError.
+  Error = Class.new(StandardError)
+
+  # Following classes inherit from the Error class.
+  # -----------------------------------------------
 
   # An Exception that is raised as a result of a parse error while parsing a JSON document.
-  class ParseError < Error
-  end # ParseError
+  ParseError = Class.new(Error)
 
   # An Exception that is raised as a result of a path being too deep.
-  class DepthError < Error
-  end # DepthError
+  DepthError = Class.new(Error)
 
   # An Exception that is raised if a file fails to load.
-  class LoadError < Error
-  end # LoadError
+  LoadError = Class.new(Error)
 
   # An Exception that is raised if there is a conflict with mimicing JSON
-  class MimicError < Error
-  end # MimicError
+  MimicError = Class.new(Error)
 
 end # Oj
