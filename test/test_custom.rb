4b7b16ce6348b3f985c7b93ba470f7e2677e3d32
Peter Ohler <peter@ohler.com>
2018-07-17 19:00:52 -0700

    Turn on trace for time test

--- test/test_custom.rb
+++ test/test_custom.rb
@@ -321,7 +321,7 @@ class CustomJuice < Minitest::Test
     dump_and_load(obj, false, :time_format => :unix, :create_id => "^o", :create_additions => true)
     dump_and_load_inspect(obj, false, :time_format => :unix_zone, :create_id => "^o", :create_additions => true)
     dump_and_load_inspect(obj, false, :time_format => :xmlschema, :create_id => "^o", :create_additions => true)
-    dump_and_load_inspect(obj, false, :time_format => :ruby, :create_id => "^o", :create_additions => true)
+    dump_and_load_inspect(obj, true, :time_format => :ruby, :create_id => "^o", :create_additions => true)
   end
 
   def dump_and_load(obj, trace=false, options={})
