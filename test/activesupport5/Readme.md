52affdb19fc49bfb7b181aeb10941106c08e8dd9
Anton Rieder <aried3r@gmail.com>
2019-10-03 11:19:01 +0200

    Use Rails 5.2.3 test cases

--- test/activesupport5/Readme.md
+++ test/activesupport5/Readme.md
@@ -1,1 +1,6 @@
-Tests copied from [rails/activesupport/test/json/](https://github.com/rails/rails/tree/v5.0.2/activesupport/test/json)
+Tests copied from [rails/activesupport/test/json/],
+[rails/activesupport/lib/active_support], [rails/activesupport/test].
+
+[rails/activesupport/test/json/]: https://github.com/rails/rails/tree/v5.2.3/activesupport/test/json
+[rails/activesupport/lib/active_support]: https://github.com/rails/rails/tree/v5.2.3/activesupport/lib/active_support
+[rails/activesupport/test]: https://github.com/rails/rails/tree/v5.2.3/activesupport/test
--- test/activesupport5/time_zone_test_helpers.rb
+++ test/activesupport5/time_zone_test_helpers.rb
@@ -1,3 +1,5 @@
+# frozen_string_literal: true
+
 module TimeZoneTestHelpers
   def with_tz_default(tz = nil)
     old_tz = Time.zone
@@ -7,11 +9,11 @@ module TimeZoneTestHelpers
     Time.zone = old_tz
   end
 
-  def with_env_tz(new_tz = 'US/Eastern')
-    old_tz, ENV['TZ'] = ENV['TZ'], new_tz
+  def with_env_tz(new_tz = "US/Eastern")
+    old_tz, ENV["TZ"] = ENV["TZ"], new_tz
     yield
   ensure
-    old_tz ? ENV['TZ'] = old_tz : ENV.delete('TZ')
+    old_tz ? ENV["TZ"] = old_tz : ENV.delete("TZ")
   end
 
   def with_preserve_timezone(value)
@@ -20,5 +22,18 @@ module TimeZoneTestHelpers
     yield
   ensure
     ActiveSupport.to_time_preserves_timezone = old_preserve_tz
+  end
+
+  def with_tz_mappings(mappings)
+    old_mappings = ActiveSupport::TimeZone::MAPPING.dup
+    ActiveSupport::TimeZone.clear
+    ActiveSupport::TimeZone::MAPPING.clear
+    ActiveSupport::TimeZone::MAPPING.merge!(mappings)
+
+    yield
+  ensure
+    ActiveSupport::TimeZone.clear
+    ActiveSupport::TimeZone::MAPPING.clear
+    ActiveSupport::TimeZone::MAPPING.merge!(old_mappings)
   end
 end
