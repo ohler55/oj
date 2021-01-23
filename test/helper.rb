678698485141b4ce70d8e351a37adb6f2911f1f7
Alan Wu <XrXr@users.noreply.github.com>
2021-01-23 08:57:27 -0500

    Fix object movement bugs (#638)
    * Move objects around to check for movement bugs
    
    * Pin objects saved to C globals in init routines

--- test/helper.rb
+++ test/helper.rb
@@ -1,3 +1,5 @@
+# frozen_string_literal: true
+#
 # Ubuntu does not accept arguments to ruby when called using env. To get warnings to show up the -w options is
 # required. That can be set in the RUBYOPT environment variable.
 # export RUBYOPT=-w
@@ -15,6 +17,14 @@ require 'date'
 require 'bigdecimal'
 require 'pp'
 require 'oj'
+
+
+if defined?(GC.verify_compaction_references) == 'method'
+  # This method was added in Ruby 3.0.0. Calling it this way asks the GC to
+  # move objects around, helping to find object movement bugs.
+  GC.verify_compaction_references(double_heap: true, toward: :empty)
+end
+
 
 $ruby = RUBY_DESCRIPTION.split(' ')[0]
 $ruby = 'ree' if 'ruby' == $ruby && RUBY_DESCRIPTION.include?('Ruby Enterprise Edition')
--- test/json_gem/test_helper.rb
+++ test/json_gem/test_helper.rb
@@ -1,3 +1,5 @@
+# frozen_string_literal: true
+
 $: << File.dirname(__FILE__)
 $oj_dir = File.dirname(File.dirname(File.expand_path(File.dirname(__FILE__))))
 %w(lib ext).each do |dir|
@@ -12,6 +14,12 @@ if ENV['REAL_JSON_GEM']
 else
   require 'oj'
   Oj.mimic_JSON
+
+  if defined?(GC.verify_compaction_references) == 'method'
+    # This method was added in Ruby 3.0.0. Calling it this way asks the GC to
+    # move objects around, helping to find object movement bugs.
+    GC.verify_compaction_references(double_heap: true, toward: :empty)
+  end
 end
 
 NaN = JSON::NaN if defined?(JSON::NaN)
