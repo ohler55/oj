7bb4b6f3503cfc44c937cf3756522f2fea37fdf6
Peter Ohler <peter@ohler.com>
2021-08-07 10:14:09 -0400

    Fiddle with CI

--- ext/oj/extconf.rb
+++ ext/oj/extconf.rb
@@ -35,7 +35,6 @@ have_func('rb_ext_ractor_safe', 'ruby.h')
 # rb_hash_bulk_insert is deep down in a header not included in normal build and that seems to fool have_func.
 have_func('rb_hash_bulk_insert', 'ruby.h') unless '2' == version[0] && '6' == version[1]
 
-
 dflags['OJ_DEBUG'] = true unless ENV['OJ_DEBUG'].nil?
 
 dflags.each do |k,v|
