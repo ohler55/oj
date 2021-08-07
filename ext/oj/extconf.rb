6efaf30bcb0a8410c8f5ec61d59f39b134cc9580
Peter Ohler <peter@ohler.com>
2021-08-07 10:07:53 -0400

    Bypass broken have_func check for Ruby 2.6.x

--- ext/oj/extconf.rb
+++ ext/oj/extconf.rb
@@ -32,12 +32,9 @@ have_func('stpcpy')
 have_func('pthread_mutex_init')
 have_func('rb_enc_associate')
 have_func('rb_ext_ractor_safe', 'ruby.h')
-have_func('rb_hash_bulk_insert', 'ruby.h')
+# rb_hash_bulk_insert is deep down in a header not included in normal build and that seems to fool have_func.
+have_func('rb_hash_bulk_insert', 'ruby.h') unless '2' == version[0] && '6' == version[1]
 
-#have_func('rb_interned_str', 'ruby.h')
-#have_func('rb_interned_str_cstr', 'ruby.h')
-#have_func('rb_enc_interned_str', 'ruby.h')
-#have_func('rb_enc_interned_str_cstr', 'ruby.h')
 
 dflags['OJ_DEBUG'] = true unless ENV['OJ_DEBUG'].nil?
 
