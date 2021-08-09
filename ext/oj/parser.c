bab11dcb7b0b84c547854087dca6ddf1ba969b3b
Peter Ohler <peter@ohler.com>
2021-08-09 18:13:18 -0400

    Remove unused include files

--- ext/oj/parser.c
+++ ext/oj/parser.c
@@ -1,24 +1,8 @@
 // Copyright (c) 2020, 2021, Peter Ohler, All rights reserved.
 
-#include "parser.h"
-
-#include <errno.h>
 #include <fcntl.h>
-#include <limits.h>
-#include <math.h>
-#include <pthread.h>
-#include <signal.h>
-#include <stdarg.h>
-#include <stdbool.h>
-#include <stdint.h>
-#include <stdio.h>
-#include <stdlib.h>
-#include <string.h>
-#include <sys/stat.h>
-#include <sys/time.h>
-#include <time.h>
-#include <unistd.h>
 
+#include "parser.h"
 #include "oj.h"
 
 #define DEBUG 0
