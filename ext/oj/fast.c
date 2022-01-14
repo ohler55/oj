d8634e918fb60a8514fd42f2d41d1c39c4796cd8
Jean Boussier <jean.boussier@gmail.com>
2022-01-14 11:35:18 +0100

    Undefine uneeded T_DATA allocators
    This is now required to avoid warnings on ruby-head.
    
    See: https://bugs.ruby-lang.org/issues/18007

--- ext/oj/fast.c
+++ ext/oj/fast.c
@@ -1719,6 +1719,7 @@ static VALUE doc_not_implemented(VALUE self) {
  */
 void oj_init_doc() {
     oj_doc_class = rb_define_class_under(Oj, "Doc", rb_cObject);
+    rb_undef_alloc_func(oj_doc_class);
     rb_define_singleton_method(oj_doc_class, "open", doc_open, 1);
     rb_define_singleton_method(oj_doc_class, "open_file", doc_open_file, 1);
     rb_define_singleton_method(oj_doc_class, "parse", doc_open, 1);
