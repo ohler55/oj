fda614f39f948babf5443b0aed106ccd49b41ca7
Shizuo Fujita <fujita@clear-code.com>
2025-02-08 17:26:30 +0900

    CI: Fix failure in test_allocate
    Since https://github.com/ruby/ruby/commit/599fbeaffa, it causes failure in test_allocate
    
    ```
    =========================================================================================================================================================================================================================================
    Failure: test_allocate(JSONExtParserTest):
      [ruby-core:35079].
      <TypeError> exception was expected but none was thrown.
    test/json_gem/json_ext_parser_test.rb:14:in 'JSONExtParserTest#test_allocate'
         11:   if defined?(JSON::Ext::Parser)
         12:     def test_allocate
         13:       parser = JSON::Ext::Parser.new("{}")
      => 14:       assert_raise(TypeError, '[ruby-core:35079]') do
         15:         parser.__send__(:initialize, "{}")
         16:       end
         17:       parser = JSON::Ext::Parser.allocate
    ```

--- test/json_gem/json_ext_parser_test.rb
+++ test/json_gem/json_ext_parser_test.rb
@@ -9,13 +9,25 @@ class JSONExtParserTest < Test::Unit::TestCase
   include Test::Unit::TestCaseOmissionSupport
 
   if defined?(JSON::Ext::Parser)
-    def test_allocate
-      parser = JSON::Ext::Parser.new("{}")
-      assert_raise(TypeError, '[ruby-core:35079]') do
+    if REAL_JSON_GEM && Gem::Version.new(RUBY_VERSION) >= Gem::Version.new('3.5.0')
+      def test_allocate
+        parser = JSON::Ext::Parser.new("{}")
         parser.__send__(:initialize, "{}")
+        assert_equal "{}", parser.source
+
+        parser = JSON::Ext::Parser.allocate
+        assert_nil parser.source
       end
-      parser = JSON::Ext::Parser.allocate
-      assert_raise(TypeError, '[ruby-core:35079]') { parser.source }
+    else
+      # Ruby 3.4.x or before
+      def test_allocate
+        parser = JSON::Ext::Parser.new("{}")
+        assert_raise(TypeError, '[ruby-core:35079]') do
+          parser.__send__(:initialize, "{}")
+        end
+        parser = JSON::Ext::Parser.allocate
+        assert_raise(TypeError, '[ruby-core:35079]') { parser.source }
+      end
     end
   end
 end
