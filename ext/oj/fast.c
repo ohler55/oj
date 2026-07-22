3014b627bfbae75ee3203821a86e3dfe17c0bf97
Peter Ohler <peter@ohler.com>
2026-07-22 14:42:29 -0400

    Fix doc path limit

--- ext/oj/fast.c
+++ ext/oj/fast.c
@@ -972,7 +972,7 @@ static void each_leaf(Doc doc, VALUE self) {
 }
 
 static int move_step(Doc doc, const char *path, int loc) {
-    if (MAX_STACK <= doc->where - doc->where_path) {
+    if (MAX_STACK <= (doc->where + 1) - doc->where_path) {
         rb_raise(rb_const_get_at(Oj, rb_intern("DepthError")), "Path too deep. Limit is %d levels.", MAX_STACK);
     }
     if ('\0' == *path) {
